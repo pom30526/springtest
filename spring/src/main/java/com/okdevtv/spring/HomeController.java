f5a4a2479e6400b6676720ff49e3b23a8503e3ea
lsm <pom30526@naver.com>
2016-02-21 00:22:55 +0900

    maria db test

--- spring/src/main/java/com/okdevtv/spring/HomeController.java
+++ spring/src/main/java/com/okdevtv/spring/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/home2.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -34,6 +34,12 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/test.do", method = RequestMethod.GET)
+	public String test(Locale locale, Model model) {
+		model.addAttribute("now", "finkle");
+		return "test";
 	}
 	
 }
